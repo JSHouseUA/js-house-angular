275ee918c24c5abb7cbdb283176f914ef505fc67
VALOVOY922 <aleksandr.valovoy1@gmail.com>
2018-04-05 22:21:22 +0300

    add material.module

---
+++ src/app/features/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import {MaterialModule} from "./material.module";
+import {NgModule} from '@angular/core';
+
+
+
+@NgModule({
+
+    imports: [
+        MaterialModule
+    ],
+    exports: [
+        MaterialModule
+    ]
+
+})
+export class SharedModule {
+}
